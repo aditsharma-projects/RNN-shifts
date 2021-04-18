49ff5dee914742a38bc6c7527b5a4aea832ac267
aditsharma-projects <ben10raceagainsttime@gmail.com>
2021-04-18 10:32:13 -0700

    Saves one-hot encoding of job_title

--- data_load.py
+++ data_load.py
@@ -19,18 +19,33 @@ def dateIndex(date):    #indexing years starting at 2000, works for sure as long
 
     return int(num)
 
-ROWS = 100000000
-data = genfromtxt('/users/facsupport/asharma/Data/pbj_full.csv',delimiter=',', skip_header = 1, dtype="i8,f8,i8,S10,S30,i8,S12",max_rows=ROWS)
-print("LOADED DATA")
-list = []
-for x in range(0,len(data)):
-    nextTuple = (data[x][0],data[x][1],data[x][2],dateIndex(str(data[x][3])),data[x][4],data[x][5],data[x][6])
-    list.append(nextTuple)
+ROWS = 250000000
 
-listSorted = sorted(list,key=itemgetter(2))
+def gen_list():
+    data = genfromtxt('/users/facsupport/asharma/Data/pbj_full.csv',delimiter=',', skip_header = 1+ROWS,dtype="i8,f8,i8,S10,S30,i8,S12",max_rows=ROWS)
+    print("LOADED DATA")
+    list = []
+    for x in range(0,len(data)):
+        nextTuple = (data[x][0],data[x][1],data[x][2],dateIndex(str(data[x][3])),str(data[x][4]),data[x][5],data[x][6])
+        list.append(nextTuple)
+        if x%1000000 == 0:
+            print(x)
+            
+    return list
+
+#one-hot encoding of job description
+def OH_Job(job_description):
+    unique_jobs = ["b'Administrator'", "b'Respiratory Therapist'", "b'Other Activities Staff'", "b'Physical Therapy Aide'", "b'Nurse Practitioner'", "b'Speech/Language Pathologist'", "b'Other Social Worker'", "b'Nurse Aide in Training'", "b'Registered Nurse Director of N'", "b'Dietitian'", "b'Other Physician'", "b'Registered Nurse with Administ'", "b'Certified Nurse Aide'", "b'Occupational Therapist'", "b'Physical Therapist'", "b'Therapeutic Recreation Special'", "b'Pharmacist'", "b'Housekeeping Service Worker'", "b'Medical Director'", "b'Qualified Activities Professio'", "b'Mental Health Service Worker'", "b'Occupational Therapy Assistant'", "b'Registered Nurse'", "b'Feeding Assistant'", "b'Qualified Social Worker'", "b'Physical Therapy Assistant'", "b'Licensed Practical/Vocational '", "b'Other Service Worker'"]
+    out = np.zeros((28))
+    for i in range(28):
+        if unique_jobs[i] == job_description:
+            out[i] = 1
+            break
+    return out
 
 
-def gen_train_example(i,j): #listSorted[i] to listSorted[j-1] inclusive are the same employee
+
+def gen_train_example(i,j,listSorted): #listSorted[i] to listSorted[j-1] inclusive are the same employee
     dataPoints = sorted(listSorted[i:j],key=itemgetter(3))
     min = dataPoints[0][3]
     max = dataPoints[len(dataPoints)-1][3]
@@ -44,47 +59,46 @@ def gen_train_example(i,j): #listSorted[i] to listSorted[j-1] inclusive are the 
             currIndex += 1
         else:
             output.append(0)
-
-    return (True,output,max-min+1)
-
-def findksequence():        
-    return
-
-dataList = []
-
-currID = listSorted[0][2]
-lastStart = 0
-lastEnd = 0
-for i in range(0,len(listSorted)):
-    lastEnd = i
-    if listSorted[i][2] != currID:
-        currID = listSorted[i][2]
-        sample = gen_train_example(lastStart,lastEnd)
-        if sample[0]:
-            seq = sample[1]
-            dataList.append(sample[1:])
-        lastStart = i
-    if i%1000000 == 0:
-        print(i)
-
-#print(dataList)
-print(type(dataList))    
-print(len(dataList))
-dataList = sorted(dataList,key=itemgetter(1))
-#print(len(dataList)
-#for i in range(0,len(dataList)):
-    #print(dataList[i][1])
-for i in range(0,len(dataList)):
-    print(dataList[i])
-
-
-dataFinal = np.ndarray(shape=(len(dataList),ONLYGENERATENUM+2))
-for i in range(len(dataList)):
-    seq = dataList[i][0]
+            
+    seq = np.array(output)
     mean = np.mean(seq)
     std = np.std(seq)
-    list = np.append((seq-mean)/std,mean)
-    dataFinal[i] = np.append(list,std)
+    seq = (seq-mean)/std
+    seq = np.append(seq,[mean,std])
+    
+    
+    return (True,np.append(seq,OH_Job(dataPoints[0][4])))
 
 
-np.savetxt("/users/facsupport/asharma/Data/batches/31/one.csv",dataFinal,delimiter=",")
+def generate_dataList():
+    listSorted = sorted(gen_list(),key=itemgetter(2))
+    
+    dataList = []
+    currID = listSorted[0][2]
+    lastStart = 0
+    lastEnd = 0
+    for i in range(0,len(listSorted)):
+        lastEnd = i
+        if listSorted[i][2] != currID:
+            currID = listSorted[i][2]
+            sample = gen_train_example(lastStart,lastEnd,listSorted)
+            if sample[0]:
+                record = sample[1]
+                dataList.append(record)
+            lastStart = i
+        if i%1000000 == 0:
+            print(i)
+     
+    return dataList
+
+dataList = generate_dataList()
+
+print(len(dataList))
+dataList = sorted(dataList,key=itemgetter(1))
+
+
+def save_Data(dataList):
+    np.savetxt("/users/facsupport/asharma/Data/batches/31/OH/two.csv",dataList,delimiter=",")
+    
+    
+save_Data(dataList)
