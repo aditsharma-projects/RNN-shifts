e00d4a638307205f36fa9d229382def73e837ae5
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2021-05-05 13:49:17 -0700

    data_prep: Add window stamping, other improvements
    - Use csv instead of pickl for info
    - Include more info about preprocessing options, timestamp
    - Allow force reload

--- data_prep.py
+++ data_prep.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import pickle
+from datetime import datetime
 
 # Generates a dictionary from Stata log file in which 'labels list' is run
 # For some column and numerically encoded val, the decoded string is:
@@ -51,7 +51,7 @@ def label_mapping_dict(file):
     return output
 
 # Generate deterministic file name from configuration
-def generate_file_names(preprocessed_dir, nrows, fill_missing_shifts, normalize):
+def generate_file_names(preprocessed_dir, nrows, fill_missing_shifts, normalize, prev_shifts):
     name = preprocessed_dir + 'pbj'
     if nrows is not None:
         name += f"_nrows_{nrows}"
@@ -59,7 +59,9 @@ def generate_file_names(preprocessed_dir, nrows, fill_missing_shifts, normalize)
         name += "_zeros"
     if normalize:
         name += "_norm"
-    return name + '.csv', name + '.pickle'
+    if prev_shifts:
+        name += f"_prev_shifts_{prev_shifts}"
+    return name + '.csv', name + '.info.csv'
 
 # Print s if conditional is truthy
 def print_if(s, conditional):
@@ -96,43 +98,74 @@ def do_fill_missing_shifts(df, verbose):
 
     return pd.concat(partitions)
 
-def initial_preprocess(raw_path, preprocessed_dir, nrows=None, fill_missing_shifts=False, verbose=True, normalize=False):
-    data_file, info_file = generate_file_names(preprocessed_dir, nrows, fill_missing_shifts, normalize)
+def do_add_prev_shifts(df, prev_shifts, verbose):
+    print_if("Adding previous shifts...", verbose)
 
-    try:
-        print_if(f"Loading preprocessed data from '{data_file}'...", verbose)
-        df = pd.read_csv(data_file)
+    for t in range(prev_shifts + 1):
+        df.insert(t, f"t_{t}", -1)
+    
+    for i in range(prev_shifts, len(df)):
+        for t in range(prev_shifts+1):
+            if df.iloc[i-t].employee_id == df.iloc[i].employee_id:
+                df.iloc[i, df.columns.get_loc(f"t_{t}")] = df["hours"].iloc[i-t]
 
-        print_if(f"Loading related info from '{info_file}'...", verbose)
-        with open(info_file, 'rb') as f:
-            info = pickle.load(f)
-        return df, info
-    except FileNotFoundError:
-        print_if("Failed.", verbose)
+    # Drop rows that do not have complete prev shift data
+    df = df[df[f"t_{prev_shifts}"] != -1]
+    return df
+
+def initial_preprocess(raw_path, preprocessed_dir, nrows=None, fill_missing_shifts=False, verbose=True, normalize=False, prev_shifts=0, force_reload = False):
+    if prev_shifts < 0:
+        raise ValueError()
+
+    data_file, info_file = generate_file_names(preprocessed_dir, nrows, fill_missing_shifts, normalize, prev_shifts)
+
+    if not force_reload:
+        try:
+            print_if(f"Loading preprocessed data from '{data_file}'...", verbose)
+            df = pd.read_csv(data_file)
+
+            print_if(f"Loading related info from '{info_file}'...", verbose)
+            info_df = pd.read_csv(info_file)
+            info = {col:info_df.iloc[0][col] for col in info_df.columns}
+            return df, info
+        except FileNotFoundError:
+            print_if("Failed.", verbose)
     
     print_if("Loading data...", verbose)
     df = pd.read_csv(raw_path, nrows=nrows, dtype={'hours':'float64'}, parse_dates = ['date'])
-    info = {}
+    info = {
+        "nrows": nrows,
+        "fill_missing_shifts": fill_missing_shifts,
+        "normalize": normalize,
+        "prev_shifts": prev_shifts,
+        "creation_date": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    }
+
     if fill_missing_shifts:
         df = do_fill_missing_shifts(df, verbose)
     
     if normalize:
         # Normalize features
-        means = {}
-        stds = {}
-        #df = df.filter(['hours', 'date_int'])
         for col in ['hours']:
-            means[col] = df[col].mean()
-            stds[col] = df[col].std()
-            df[col] = (df[col] - means[col]) / stds[col]
-        info['means'] = means
-        info['stds'] = stds
+            mean = df[col].mean()
+            std = df[col].std()
+            info[f"means.{col}"] = mean
+            info[f"stds.{col}"] = std
+            df[col] = (df[col] - mean) / std
+    
+    if prev_shifts:
+        df = do_add_prev_shifts(df, prev_shifts, verbose)
 
     print_if("Saving preprocessed data...", verbose)
     df.to_csv(data_file, index=False)
 
-    with open(info_file, 'wb') as handle:
-        pickle.dump(info, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    info_cols = []
+    info_row = []
+    for k, v in info.items():
+        info_cols.append(k)
+        info_row.append(v)
+    info_df = pd.DataFrame([info_row], columns = info_cols)
+    info_df.to_csv(info_file, index=False)
     
     print_if("Preprocessing finished.", verbose)
     return df, info
