12b30112539c1534f77acdbcfe69fd9aad48d1c2
aditsharma-projects <ben10raceagainsttime@gmail.com>
2021-07-22 12:38:20 -0700

    first commit

---
+++ rnn/rnn_predict.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from models import RNN
+import os.path
+import json
+
+SET = "B"
+
+LAGGED_DAYS = 60
+BUFFER_SIZE = 10000
+BATCH_SIZE = 256
+
+
+if SET == "A":
+  steps = 555919099/BATCH_SIZE
+  PATH = '/export/storage_adgandhi/PBJhours_ML/tf_data/50A.csv'
+elif SET == "B":
+  steps = 557000804/BATCH_SIZE
+  PATH = '/export/storage_adgandhi/PBJhours_ML/tf_data/50B.csv'
+
+
+LOG_PATH = '/users/facsupport/asharma/RNN-shifts/output/rnn_autotuning_history.csv'
+checkpoint_path = "./"+SET+"_checkpoints/cp-{epoch:04d}.ckpt"
+CHECKPOINT_DIR = os.path.dirname(checkpoint_path)
+
+logs = pd.read_csv(LOG_PATH)
+logs = logs.loc[logs['Embedding Dimension']==0]
+LSTM_UNITS = logs.iloc[0]["LSTM Units"]
+SHAPE = json.loads(logs.iloc[0]["FF model shape"])
+
+
+dataset = tf.data.experimental.make_csv_dataset(
+    PATH,
+    batch_size=BATCH_SIZE,
+    label_name='hours'
+)
+
+def pack(features, label):
+  description = [tf.strings.as_string(features.pop('employee_id',None)),tf.strings.as_string(features['prov_id']),features.pop('date',None)]
+  lst = list(features.values())
+  return tf.stack(description,axis=1), tf.stack([tf.cast(i,tf.float32) for i in lst], axis=-1), label
+
+dataset = dataset.map(pack)
+
+model = RNN(LAGGED_DAYS,LSTM_UNITS,SHAPE,0,1)
+
+model.compile(loss=tf.keras.losses.MeanSquaredError(),
+        optimizer=tf.keras.optimizers.Adam(),
+        metrics=[tf.keras.metrics.MeanAbsoluteError()])
+
+model.load_weights(tf.train.latest_checkpoint(CHECKPOINT_DIR))
+
+outList = []
+i=0
+for descriptions,features,labels in dataset:
+    i+=1
+    if i%10000 == 0:
+        print(i)
+    predictions = model(features)
+    outList.append(tf.concat([descriptions,tf.strings.as_string(predictions),tf.strings.as_string(tf.expand_dims(labels,axis=1))],axis=1))
+
+
+final_tensor = tf.concat(outList,axis=0)
+np.savetxt('/export/storage_adgandhi/PBJhours_ML/Data/Predictions/'+SET+'pred.csv',final_tensor.numpy(),header="employee_id,prov_id,date,prediction,hours",fmt='%s')
