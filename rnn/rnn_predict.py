0319b1d2c7e7d8200c411882b965f2888e8b7e5a
aditsharma-projects <ben10raceagainsttime@gmail.com>
2021-10-05 19:49:24 -0700

    Bug fix, explicitly declare datatype for prov_id as string

--- rnn/rnn_predict.py
+++ rnn/rnn_predict.py
@@ -6,8 +6,8 @@ import os.path
 import json
 import time
 
-SET = "B"
-NET = "A"
+SET = "A"
+NET = "B"
 
 LAGGED_DAYS = 60
 BUFFER_SIZE = 10000
@@ -17,16 +17,16 @@ BATCH_SIZE = 2048*8
 if SET == NET:
   print("Warning, running predictions on train set")
 
-
 if SET == "A":
-  steps = int(555919099/BATCH_SIZE)
   PATH = '/export/storage_adgandhi/PBJhours_ML/tf_data/50A.csv'
+  #steps = int(len(pd.read_csv(PATH,usecols=['hours']))/BATCH_SIZE)
+  steps = 10000000
 elif SET == "B":
-  steps = int(557000804/BATCH_SIZE)
   PATH = '/export/storage_adgandhi/PBJhours_ML/tf_data/50B.csv'
+  steps = int(len(pd.read_csv(PATH,usecols=['hours']))/BATCH_SIZE)
 
 
-LOG_PATH = '/users/facsupport/asharma/RNN-shifts/output/rnn_autotuning_history.csv'
+LOG_PATH = '/export/storage_adgandhi/PBJhours_ML/Data/Logs/rnn_autotuning_history.csv'
 checkpoint_path = "./"+NET+"_checkpoints/cp-{epoch:04d}.ckpt"
 CHECKPOINT_DIR = os.path.dirname(checkpoint_path)
 
@@ -35,16 +35,19 @@ logs = logs.loc[logs['Embedding Dimension']==0]
 LSTM_UNITS = logs.iloc[0]["LSTM Units"]
 SHAPE = json.loads(logs.iloc[0]["FF model shape"])
 
-
+d_types = ['float32']+['string']*5+['float32']*128
 dataset = tf.data.experimental.make_csv_dataset(
     PATH,
     batch_size=BATCH_SIZE,
-    label_name='hours'
+    label_name='hours',
+    column_defaults=d_types
 )
 
 def pack(features, label):
-  description = [tf.strings.as_string(features.pop('employee_id',None)),tf.strings.as_string(features['prov_id']),features.pop('date',None),
-                 tf.strings.as_string(features.pop('job_title',None)),tf.strings.as_string(features.pop('pay_type',None))]
+  #description = [tf.strings.as_string(features.pop('employee_id',None)),tf.strings.as_string(features.pop('prov_id',None)),tf.strings.as_string(features.pop('date',None)),
+  #               tf.strings.as_string(features.pop('job',None)),tf.strings.as_string(features.pop('pay_type',None))]
+  #description = [tf.strings.as_string(features.pop('employee_id',None)),tf.strings.as_string(features.pop('prov_id',None)),features.pop('date',None),features.pop('job',None),features.pop('pay_type',None)]
+  description = [features.pop('employee_id',None),features.pop('prov_id',None),features.pop('date',None),features.pop('job',None),features.pop('pay_type',None)]
   lst = list(features.values())
   return tf.stack(description,axis=1), tf.stack([tf.cast(i,tf.float32) for i in lst], axis=-1), label
 
@@ -59,10 +62,8 @@ model.compile(loss=tf.keras.losses.MeanSquaredError(),
 model.load_weights(tf.train.latest_checkpoint(CHECKPOINT_DIR))
 
 outList = []
-i=0
 start_time = time.time()
-for descriptions,features,labels in dataset:
-    i+=1
+for i, (descriptions,features,labels) in enumerate(dataset):
     if i%1000 == 10:
         print(f"Completed {i} batches. Last {1000} batches took {time.time()-start_time} seconds. {100*i/steps} % done")
         start_time = time.time()
@@ -84,5 +85,5 @@ for col in ['employee_id','prov_id','date','job_title','pay_type']:
   frame[col] = frame[col].str.decode('utf-8')
 
 frame[['prediction','hours']] = frame[['prediction','hours']].apply(pd.to_numeric)
-frame.to_csv('/export/storage_adgandhi/PBJhours_ML/Data/Predictions/RNN_60days/tmp/'+SET+'pred.csv',index=False)
+frame.to_csv('/export/storage_adgandhi/PBJhours_ML/Data/Predictions/RNN_60days/Latest_Data/'+SET+'pred.csv',index=False)
 print(f"MSE: {((frame.prediction-frame.hours)**2).mean()}")
